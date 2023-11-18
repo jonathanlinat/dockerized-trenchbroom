75c9a9068d794a1d09838441d68bcf085aca5095
Jonathan Linat <jonathan.linat@gmail.com>
2023-11-18 09:50:41 -0500

    chore: tweak the Docker pipeline

--- build.sh
+++ build.sh
@@ -38,23 +38,32 @@ CONTAINER_NAME="${IMAGE_NAME}-container"
 # Build the Docker image
 DOCKER_BUILDKIT=1 docker build -f ./Dockerfile.$BUILD_ENV -t $IMAGE_NAME --build-arg BUILD_ENV=$BUILD_ENV --build-arg BUILD_TYPE=$BUILD_TYPE --build-arg BUILD_TARGET=$BUILD_TARGET .
 
-# Check if the container already exists
+# Check if the container already exists; if so, delete it
+if [ $(docker ps -a -q -f name=^/$CONTAINER_NAME$) ]; then
+  docker rm $CONTAINER_NAME
+fi
+
+# Create a new container
 docker run -d --name $CONTAINER_NAME $IMAGE_NAME
 
-# Define output path
-BUILD_PATH=TrenchBroom/build/$BUILD_ENV/$BUILD_TYPE/$BUILD_TARGET
+# Define output paths
+BUILD_SOURCE_PATH=TrenchBroom/build/$BUILD_ENV/$BUILD_TYPE/$BUILD_TARGET
+BUILD_DESTINATION_PATH=$BUILD_SOURCE_PATH
 
-# Delete local app directory
-rm -rf ./$BUILD_PATH
+# Check if the folder exists, if so, delete it
+rm -rf "./$BUILD_DESTINATION_PATH"
 
 # Ensure local build directory exists, create it if it does not
-mkdir -p ./$BUILD_PATH
+mkdir -p ./$BUILD_DESTINATION_PATH
 
-# Copy files from the container to the host
-docker cp $CONTAINER_NAME:/$BUILD_PATH/. ./$BUILD_PATH
+# Loop over each file and copy it
+FILES_TO_COPY=(".")
+for file in "${FILES_TO_COPY[@]}"; do
+  docker cp "$CONTAINER_NAME:/$BUILD_SOURCE_PATH/$file" "./$BUILD_DESTINATION_PATH/$file"
+done
 
 # Stop and remove the container
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
-echo "Building process of ${BUILD_TARGET} (${BUILD_ENV}) completed successfully!"
+echo "Building process of TrenchBroom (${BUILD_TARGET} - ${BUILD_ENV}) completed successfully!"
